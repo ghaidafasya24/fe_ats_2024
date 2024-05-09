af10958aae7cb1891030de32efb71c872be697cd
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-05-09 23:41:15 +0700

    yaallah hamba lelah

--- js/fetch.js
+++ js/fetch.js
@@ -9,10 +9,9 @@ function GetAllResponden(results) {
 }
 function isiRow(value) {
     let content = 
-    isiTabel.replace("#JENISKELAMIN#", value.TugasWeek04_Kuisoner.jenis_kelamin)
-            .replace("#PROGRAMSTUDI#", value.TugasWeek04_Kuisoner.program_studi)
-            .replace("#PERTANYAAN#", value.TugasWeek04_Kuisoner.pertanyaan)
-            .replace("#JAWABAN#", value.TugasWeek04_Kuisoner.jawaban)
+    isiTabel.replace("#USIA#", value.usia)
+            .replace("#JENISKELAMIN#", value.jenis_kelamin)
+            .replace("#PROGRAMSTUDI#", value.program_studi)
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
         addInner("iniTabel", content);
@@ -31,10 +30,9 @@ function isiRow(value) {
 //     console.log("Value:", value); //Tampilkan nilai value di konsol
 //     if (value && value.jenis_kelamin && value.program_studi && value.pertanyaan && value.jawaban) {
 //         let content = 
-//         isiTabel.replace("#JENISKELAMIN#", value.jenis_kelamin)
+//         isiTabel.replace("#USIA#", value.usia)
+//                 .replace("#JENISKELAMIN#", value.jenis_kelamin)
 //                 .replace("#PROGRAMSTUDI#", value.program_studi)
-//                 .replace("#PERTANYAAN#", value.pertanyaan)
-//                 .replace("#JAWABAN#", value.jawaban)
 //                 .replace("#WARNA#", getRandomColor())
 //                 .replace(/#WARNALOGO#/g, getRandomColorName());
 //         addInner("iniTabel", content);
