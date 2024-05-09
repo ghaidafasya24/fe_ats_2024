8bcc6f638943b3374dfada9c4d420876f0eba819
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-05-09 22:21:26 +0700

    yaallah plissss

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,49 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = "https://ws-ats-714220031-f2558a9a9f9c.herokuapp.com/responden";
-get(urlAPI,GetAllResponden);
-function GetAllResponden(results){
-    console.log(results);
+get(urlAPI, GetAllResponden);
+function GetAllResponden(results) {
+    results.forEach(isiRow);
 }
+function isiRow(value) {
+    let content = 
+    isiTabel.replace("#JENISKELAMIN#", value.TugasWeek04_Kuisoner.jenis_kelamin)
+            .replace("#PROGRAMSTUDI#", value.TugasWeek04_Kuisoner.program_studi)
+            .replace("#PERTANYAAN#", value.TugasWeek04_Kuisoner.pertanyaan)
+            .replace("#JAWABAN#", value.TugasWeek04_Kuisoner.jawaban)
+            .replace("#WARNA#", getRandomColor())
+            .replace(/#WARNALOGO#/g, getRandomColorName());
+        addInner("iniTabel", content);
+}
+
+// import { get } from "https://bukulapak.github.io/api/process.js";
+// import { addInner } from "https://bukulapak.github.io/element/process.js";
+// import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+// import { isiTabel } from "./table.js";
+// let urlAPI = "https://ws-ats-714220031-f2558a9a9f9c.herokuapp.com/responden";
+// get(urlAPI, GetAllResponden);
+// function GetAllResponden(results) {
+//     results.forEach(isiRow);
+// }
+// function isiRow(value) {
+//     console.log("Value:", value); //Tampilkan nilai value di konsol
+//     if (value && value.jenis_kelamin && value.program_studi && value.pertanyaan && value.jawaban) {
+//         let content = 
+//         isiTabel.replace("#JENISKELAMIN#", value.jenis_kelamin)
+//                 .replace("#PROGRAMSTUDI#", value.program_studi)
+//                 .replace("#PERTANYAAN#", value.pertanyaan)
+//                 .replace("#JAWABAN#", value.jawaban)
+//                 .replace("#WARNA#", getRandomColor())
+//                 .replace(/#WARNALOGO#/g, getRandomColorName());
+//         addInner("iniTabel", content);
+    
+//     } else {
+//         console.error("Data Responden tidak lengkap:", value);
+//     }
+    
+// }
+
+
+
---
+++ js/table.js
@@ -0,0 +1,42 @@
+export let isiTabel =
+`
+<tr
+class="whitespace-nowrap h-11 bg-coolGray-50 bg-opacity-80 border-b border-coolGray-100"
+>
+<th
+  class="px-4 font-semibold text-xs text-coolGray-500 uppercase text-left"
+>
+  <div class="flex items-center">
+    <input
+      class="mr-3 w-4 h-4 bg-white rounded"
+      type="checkbox"
+    />
+    <p>ID RESPONDEN</p>
+  </div>
+</th>
+<th
+  class="whitespace-nowrap px-4 font-semibold text-xs text-coolGray-500 uppercase text-left"
+>
+  JENIS KELAMIN
+</th>
+<th
+  class="whitespace-nowrap px-4 font-semibold text-xs text-coolGray-500 uppercase text-center"
+>
+  PROGRAM STUDI
+</th>
+<th
+  class="whitespace-nowrap px-4 font-semibold text-xs text-coolGray-500 uppercase text-left"
+>
+  PERTANYAAN
+</th>
+<th
+  class="whitespace-nowrap px-4 font-semibold text-xs text-coolGray-500 uppercase text-left"
+>
+  JAWABAN
+</th>
+
+<th
+  class="whitespace-nowrap font-semibold text-xs text-coolGray-500 uppercase text-center"
+></th>
+</tr>
+`
