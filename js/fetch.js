47dcd1db654792ec8ea55352a5d83f5453bbd0d1
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-05-10 00:43:56 +0700

    bismillah muncul

--- js/fetch.js
+++ js/fetch.js
@@ -7,15 +7,34 @@ get(urlAPI, GetAllResponden);
 function GetAllResponden(results) {
     results.forEach(isiRow);
 }
+
 function isiRow(value) {
     let content = 
-    isiTabel.replace("#USIA#", value.usia)
-            .replace("#JENISKELAMIN#", value.jenis_kelamin)
-            .replace("#PROGRAMSTUDI#", value.program_studi)
+    isiTabel.replace("#USIA#", value.TugasWeek04_Kuisoner.usia)
+            .replace("#JENISKELAMIN#", value.TugasWeek04_Kuisoner.jenis_kelamin)
+            .replace("#PROGRAMSTUDI#", value.TugasWeek04_Kuisoner.program_studi)
             .replace("#WARNA#", getRandomColor())
             .replace(/#WARNALOGO#/g, getRandomColorName());
         addInner("iniTabel", content);
 }
+// function isiRow(value) {
+//     console.log("Value:", value); // Tampilkan nilai value di konsol
+//     if (value && value.usia && value.jenis_kelamin && value.program_studi) {
+//         let content = 
+//             isiTabel.replace("#USIA#", value.usia)
+//                     .replace("#JENISKELAMIN#", value.jenis_kelamin)
+//                     .replace("#PROGRAMSTUDI#", value.program_studi)
+//                     .replace("#WARNA#", getRandomColor())
+//                     .replace(/#WARNALOGO#/g, getRandomColorName());
+//         addInner("iniTabel", content);
+//     } else {
+//         console.error("Data mahasiswa tidak lengkap:", value);
+//     }
+// }
+
+
+
+
 
 // import { get } from "https://bukulapak.github.io/api/process.js";
 // import { addInner } from "https://bukulapak.github.io/element/process.js";
