7579451a0de55983e62ae44dc99c578cdfbdf584
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-05-10 18:29:47 +0700

    rapihin

--- js/fetch.js
+++ js/fetch.js
@@ -1,9 +1,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
 import {GetAllResponden } from "./controller/get.js";
 import { urlAPI } from "./config/url.js";
-// import { urlAPI2 } from "./config/url.js";
 get(urlAPI,GetAllResponden);
-// get(urlAPI2,GetAllPertanyaan);
 
 
 
